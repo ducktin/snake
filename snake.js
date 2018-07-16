d5b376eccb5a3697ec59892a05e7715a27ddaf95
Martin Borbola <borbolam14@gmail.com>
2018-07-16 21:48:30 +0200

    Abstract out the snake position

--- snake.js
+++ snake.js
@@ -1,32 +1,41 @@
+var gameCanvas = document.getElementById("gameCanvas");
+
 const CANVAS_BORDER_COLOUR = 'black';
 const CANVAS_BACKGROUND_COLOUR = "white";
+
+const CELL_WIDTH = 20;
+const CELL_HEIGHT = 20;
+
+const CANVAS_CELL_WIDTH = gameCanvas.width / CELL_WIDTH;
+const CANVAS_CELL_HEIGHT = gameCanvas.height / CELL_HEIGHT;
+
 const SNAKE_COLOUR = 'lightgreen';
 const SNAKE_BORDER_COLOUR = 'darkgreen';
 
+
 let snake = [{
-        x: 150,
-        y: 150
+        x: 8,
+        y: 4
     },
     {
-        x: 140,
-        y: 150
+        x: 7,
+        y: 4
     },
     {
-        x: 130,
-        y: 150
+        x: 6,
+        y: 4
     },
     {
-        x: 120,
-        y: 150
+        x: 5,
+        y: 4
     },
     {
-        x: 110,
-        y: 150
-    },
+        x: 4,
+        y: 4
+    }
 ];
 
 // Get the canvas element
-var gameCanvas = document.getElementById("gameCanvas");
 // Return a two dimensional drawing context
 var ctx = gameCanvas.getContext("2d");
 //  Select the colour to fill the canvas
@@ -42,8 +51,8 @@ function drawSnakePart(snakePart) {
     ctx.fillStyle = 'lightgreen';
     ctx.strokestyle = 'darkgreen';
 
-    ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
-    ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+    ctx.fillRect(snakePart.x * CELL_WIDTH, snakePart.y * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT);
+    ctx.strokeRect(snakePart.x * CELL_WIDTH, snakePart.y * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT);
 }
 
 function drawSnake() {
