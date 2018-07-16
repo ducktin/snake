d844bf3ef8a70a702a02cdddc3564e9732551f3f
Martin Borbola <borbolam14@gmail.com>
2018-07-16 21:38:42 +0200

    Created separate snake.js file

---
+++ snake.js
@@ -0,0 +1,51 @@
+const CANVAS_BORDER_COLOUR = 'black';
+const CANVAS_BACKGROUND_COLOUR = "white";
+const SNAKE_COLOUR = 'lightgreen';
+const SNAKE_BORDER_COLOUR = 'darkgreen';
+
+let snake = [{
+        x: 150,
+        y: 150
+    },
+    {
+        x: 140,
+        y: 150
+    },
+    {
+        x: 130,
+        y: 150
+    },
+    {
+        x: 120,
+        y: 150
+    },
+    {
+        x: 110,
+        y: 150
+    },
+];
+
+// Get the canvas element
+var gameCanvas = document.getElementById("gameCanvas");
+// Return a two dimensional drawing context
+var ctx = gameCanvas.getContext("2d");
+//  Select the colour to fill the canvas
+ctx.fillStyle = CANVAS_BACKGROUND_COLOUR;
+//  Select the colour for the border of the canvas
+ctx.strokestyle = CANVAS_BORDER_COLOUR;
+// Draw a "filled" rectangle to cover the entire canvas
+ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+// Draw a "border" around the entire canvas
+ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+
+function drawSnakePart(snakePart) {
+    ctx.fillStyle = 'lightgreen';
+    ctx.strokestyle = 'darkgreen';
+
+    ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
+    ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+
+function drawSnake() {
+    snake.forEach(drawSnakePart);
+}
