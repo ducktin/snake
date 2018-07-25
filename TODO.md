c4ada0b73b519b0e15d6c13098637b3167cbaead
Martin Borbola <borbolam14@gmail.com>
2018-07-26 00:22:19 +0200

    commit this

--- TODO.md
+++ TODO.md
@@ -4,8 +4,8 @@
 - game Object
 - ~~turn snake~~
 - ~~pause game~~
-- detect hit wall and self
-- spawn fruit
-- snake eat fruit
-- snake add point
+- ~~detect hit wall and self~~
+- ~~spawn fruit~~
+- ~~snake eat fruit~~
+- ~~snake add point~~
 - highscore table in indexDB, localstorage, sessionstorage? (show table on the side)
--- snake.js
+++ snake.js
@@ -1,5 +1,6 @@
 var gameCanvas = document.getElementById("gameCanvas");
 var ctx = gameCanvas.getContext("2d");
+var pointsTxt = document.getElementById("points");
 
 const UP = {
     x: 0,
@@ -29,9 +30,10 @@ const CANVAS_CELL_HEIGHT = gameCanvas.height / CELL_HEIGHT;
 
 const SNAKE_COLOUR = 'lightgreen';
 const SNAKE_BORDER_COLOUR = 'darkgreen';
+const FRUIT_COLOUR = 'red';
+const FRUIT_BORDER_COLOUR = 'black';
 
-const GAME_TICK = 200;
-
+const GAME_TICK = 100;
 
 let snake = [{
         x: 8,
@@ -54,17 +56,18 @@ let snake = [{
         y: 4
     }
 ];
+var fruit;
 var direction = RIGHT;
 var paused = false;
+var points = 0;
 
 setupGame();
 move();
 
 function setupGame() {
     setupEventListeners();
-    clearCanvas();
 
-    drawSnake();
+    spawnFruit();
 }
 
 function move() {
@@ -74,6 +77,7 @@ function move() {
             move();
         }
     }, GAME_TICK);
+    console.log('timeout set');
 }
 
 function setupEventListeners() {
@@ -83,34 +87,44 @@ function setupEventListeners() {
 
 function moveEventHandler(event) {
     if (event.key === "w") {
-        direction = UP;
+        if(direction !== DOWN){
+            direction = UP;
+        }
     } else if (event.key === "a") {
+        if(direction !== RIGHT){
         direction = LEFT;
+        }
     } else if (event.key === "s") {
-        direction = DOWN;
+        if(direction !== UP){
+            direction = DOWN;
+        }
     } else if (event.key === "d") {
+        if(direction !== LEFT){
         direction = RIGHT;
+        }
     }
 }
 
 function pauseEventHandler(event) {
     if (event.key === " ") {
-        pausePressed();
+        pauseGame();
     }
 }
 
-function pausePressed() {
+function pauseGame() {
     if (paused) {
+        console.log("game continued");
         paused = false;
         move();
     } else {
+        console.log("game paused");
         paused = true;
     }
 }
 
 function clearCanvas() {
     ctx.fillStyle = CANVAS_BACKGROUND_COLOUR;
-    ctx.strokestyle = CANVAS_BORDER_COLOUR;
+    ctx.strokeStyle = CANVAS_BORDER_COLOUR;
 
     ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
     ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
@@ -118,7 +132,7 @@ function clearCanvas() {
 
 function drawSnakePart(snakePart) {
     ctx.fillStyle = SNAKE_COLOUR;
-    ctx.strokestyle = SNAKE_BORDER_COLOUR;
+    ctx.strokeStyle = SNAKE_BORDER_COLOUR;
 
     ctx.fillRect(snakePart.x * CELL_WIDTH, snakePart.y * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT);
     ctx.strokeRect(snakePart.x * CELL_WIDTH, snakePart.y * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT);
@@ -128,20 +142,102 @@ function drawSnake() {
     snake.forEach(drawSnakePart);
 }
 
+function drawFruit() {
+    ctx.fillStyle = FRUIT_COLOUR;
+    ctx.strokeStyle = FRUIT_BORDER_COLOUR;
+
+    var circleCenter = {
+        x: fruit.x * CELL_WIDTH + CELL_WIDTH/2,
+        y: fruit.y * CELL_HEIGHT + CELL_HEIGHT/2
+    }
+
+    ctx.beginPath();
+    ctx.arc(circleCenter.x, circleCenter.y, CELL_WIDTH/2-2, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
+}
+
 function moveSnake(direction) {
     advanceSnake(direction.x, direction.y);
     clearCanvas();
     drawSnake();
+    drawFruit();
 }
 
 function advanceSnake(xVelocity, yVelocity) {
-
+    // we move the snake by creating a new head in the movement direction, and then removing the tail from the end
     const head = {
         x: snake[0].x + xVelocity,
         y: snake[0].y + yVelocity
     };
+    if (checkHit(head)) {
+        gameOver();
+    } else {
+        if(checkFruitHit(head)){
+            fruitHit();
+        }
+        snake.unshift(head);
+        snake.pop();
+    }
+}
 
-    snake.unshift(head);
+function checkHit(head) {
+    return checkSelfHit(head) || checkWallHit(head);
+}
 
-    snake.pop();
+function checkSelfHit(head) {
+    for (const element of snake) {
+        if (head.x == element.x && head.y == element.y) {
+            console.log("self hit");
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkWallHit(head) {
+    if (head.x >= 0 && head.x < CANVAS_CELL_WIDTH && head.y >= 0 && head.y < CANVAS_CELL_HEIGHT ) {
+        return false;
+    }
+    else {
+        console.log("wall hit");
+        return true;
+    }
+}
+
+function checkFruitHit(head) {
+    if (head.x == fruit.x && head.y == fruit.y) {
+        console.log("fruit hit");
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function fruitHit() {
+    addPoints(10);
+    spawnFruit();
+}
+
+function spawnFruit() {
+    var pos = getRandomPosition();
+    console.log(`fruit spawned at x:${pos.x}, y:${pos.y}`);
+    fruit = pos;
+}
+
+function getRandomPosition(){
+    return {
+        x: Math.floor(Math.random()*CANVAS_CELL_WIDTH),
+        y: Math.floor(Math.random()*CANVAS_CELL_HEIGHT)
+    }
+}
+
+function addPoints(plusPoints) {
+    points += plusPoints;
+    pointsTxt.innerHTML = points;
+}
+
+function gameOver() {
+    pauseGame();
+    // TODO
 }
